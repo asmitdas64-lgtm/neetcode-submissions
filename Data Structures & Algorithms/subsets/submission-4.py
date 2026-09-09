affaf61f3b062e4c35ce719a6c85badb04b852ae
Asmit Das <asmitdas64@gmail.com>
2026-09-09 12:53:20 +0530

    Add: subsets - submission-4

---
+++ Data Structures & Algorithms/subsets/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        sol = []
+
+        n = len(nums)
+
+        def dfs(i):
+            if i == n:
+                res.append(sol[:])
+                return 
+            
+            dfs(i+1)
+
+
+            sol.append(nums[i])
+            dfs(i+1)
+            sol.pop()
+        
+
+        dfs(0)
+        return res
